5564509bbc3b77a339ac5ece0111f7d1bac3c079
Jack Merrill <me@jackmerrill.com>
2021-06-12 01:14:21 -0500

    Initial Commit

---
+++ SCP Mobile/SCP_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCP_MobileApp.swift
+//  SCP Mobile
+//
+//  Created by Jack Merrill on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SCP_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
